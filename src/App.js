e3a250b34bc7a925b5131462dbc8ed12c9754aaf
Natalia Rodriguez <nc.rodriguez206@gmail.com>
2021-05-03 18:37:24 -0500

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ElementList from './components/elements';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hola Soy nati</p>
+      <div className="App__container">
+
+      <header role="banner" className="Header Header--color " > 
+      <nav>
+      <div className="Header__elements">
+        <label for="searchObject" className="nabvar__search"> Nunca dejes de buscar <input type="text" role="search"/> <button className="search__button"></button></label>
+        </div>
+      </nav>
       </header>
 
-      <body>
-        <nav>
-
-        </nav>
+      <body className="Content">
         <main>
+          <section className="Information__element Direction__wrap"> 
+          <div className="Information__image"></div> 
+          <div className="Information__text Direction__column Text"><h2>Titulo </h2> <p className="Text--margin"> Texto informativo del objeto </p></div> 
+          <div>
+            <p>precio</p>
+          </div>
+          </section>
+          
           <section>
-          <p>Aca va la imagen del producto</p>
+          <p>SECTION IMG</p>
             <article><p>aca va el texto del producto</p></article>
           </section>
-          <section><p>no se si es aside pero aca estara el precio </p></section>
+          <section ><p>SECTION - precio </p></section>
+        
         </main>
+        
         <footer>
 
         </footer>
       </body>
-      
+      </div>
     </div>
   
 
---
+++ src/components/elements.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function ElementList(props) {
+let list 
+
+    return(
+    <div>
+        <ul className="List__element">
+            {list}
+        </ul>
+    </div>);
+
+}
+
+export default ElementList;
